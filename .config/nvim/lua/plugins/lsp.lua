8fbcc97e389b848be128d328291190c0d790ec53
Wansoo Kim <rladhkstn8@gmail.com>
2024-01-26 16:10:01 +0900

    add

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -288,4 +288,15 @@ return {
 			autocmd = { enabled = true },
 		},
 	},
+	{
+		'nvimdev/lspsaga.nvim',
+		event = "LspAttach",
+		config = function()
+			require('lspsaga').setup({})
+		end,
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter',
+			'nvim-tree/nvim-web-devicons',
+		}
+	}
 }
